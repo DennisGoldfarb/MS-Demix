2057d9cb19b04c3feba684dda7f0e343376da8cc
DennisGoldfarb <goldfarb.dennis@gmail.com>
2018-03-03 12:17:06 -0500

    writing scan details

--- ProcessSingleSpectrum.cpp
+++ ProcessSingleSpectrum.cpp
@@ -10,9 +10,9 @@
 #include "Util.h"
 #include "NNLSModel.h"
 
-void usage()
-{
-    std::cout << "usage: ProcessSingleSpectrum mzML_file scan_ID numScans minZ maxZ maxIso output_directory" << std::endl;
+void usage() {
+    std::cout << "usage: ProcessSingleSpectrum mzML_file scan_ID numScans minZ maxZ maxIso output_directory"
+              << std::endl;
     std::cout << "\tmzML_file: path to input .mzML file " << std::endl;
     std::cout << "\tscan_ID: first scan_ID to process." << std::endl;
     std::cout << "\tnumScans: number of scans to process." << std::endl;
@@ -22,8 +22,16 @@ void usage()
     std::cout << "\toutput_directory: path to output files" << std::endl;
 }
 
-int main(int argc, char * argv[])
+void writeScan(OpenMS::MSSpectrum &scan, std::string outPath, int scanID, double monoMass, int z)
 {
+    std::ofstream out(outPath + std::to_string(scanID) + ".tab");
+
+    out << scan.getName() << "\t" << scan.getSourceFile() << "\t" << scanID << "\t" << monoMass << "\t" << z << "\t" << scan.getRT() << std::endl;
+
+    out.close();
+}
+
+int main(int argc, char *argv[]) {
     //check for correct number of command line arguments
     if (argc != 8) {
         usage();
@@ -44,36 +52,37 @@ int main(int argc, char * argv[])
     //OpenMS::PeakMap map;
     OpenMS::IndexedMzMLFileLoader mzMLDataFile;
     OpenMS::OnDiscPeakMap map;
-    
+
     mzMLDataFile.load(mzMLPath, map);
 
     for (int i = scanID; i < scanID + numScans; ++i)
     {
         OpenMS::MSSpectrum scan = map.getSpectrum(i);
 
-        if (scan.getMSLevel() == 2) 
-	{
-	    std::cout << "MS2 scan: " << i << std::endl;
-            
-	    const OpenMS::Precursor precursorInfo = scan.getPrecursors()[0];
+        if (scan.getMSLevel() == 2)
+        {
+            std::cout << "MS2 scan: " << i << std::endl;
+
+            const OpenMS::Precursor precursorInfo = scan.getPrecursors()[0];
 
             double isolationWidth = precursorInfo.getIsolationWindowUpperOffset() + precursorInfo.getIsolationWindowLowerOffset();
-	    double isolationCenter = precursorInfo.getMZ();
-	    double monoMass = (isolationCenter - OpenMS::Constants::C13C12_MASSDIFF_U) * precursorInfo.getCharge();
-	    double isotopeSpacing = 1.0 / precursorInfo.getCharge();
-	    int maxIsolatedIso = isolationWidth/2 / isotopeSpacing;
+            double isolationCenter = precursorInfo.getMZ();
+            double monoMass = (isolationCenter - OpenMS::Constants::C13C12_MASSDIFF_U) * precursorInfo.getCharge();
+            double isotopeSpacing = 1.0 / precursorInfo.getCharge();
+            int maxIsolatedIso = isolationWidth / 2 / isotopeSpacing;
 
-	    PrecursorTargetOption target(monoMass, monoMass, precursorInfo.getCharge(), 0, maxIsolatedIso, 1.0);
+            PrecursorTargetOption target(monoMass, monoMass, precursorInfo.getCharge(), 0, maxIsolatedIso, 1.0);
 
-	    PrecursorTargetOptions options;
-	    options.addOption(target);
+            PrecursorTargetOptions options;
+            options.addOption(target);
 
-	    Util::populateOptionsForIsolationWindow(minZ, maxZ, maxIso, charge2prob, isolationWidth, isolationCenter, options);
+            Util::populateOptionsForIsolationWindow(minZ, maxZ, maxIso, charge2prob, isolationWidth, isolationCenter, options);
 
-	    NNLSModel model(scan, options, 20, MassToleranceUnit::PPM);
+            NNLSModel model(scan, options, 20, MassToleranceUnit::PPM);
 
-	    model.writeModel(outPath, std::to_string(i));
-	}
+            model.writeModel(outPath, std::to_string(i));
+            writeScan(scan, outPath, i, monoMass, precursorInfo.getCharge());
+        }
     }
 
     return 0;
